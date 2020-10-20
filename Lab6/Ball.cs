970c4067db4f4ae7044ffb88f1e0c71482f68864
Polina Alekseeva <72174553+alexpolina@users.noreply.github.com>
2020-10-20 20:31:10 +0300

    Lab6
    Лабораторная работа №6

---
+++ Lab6/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Lab6
+{
+    class Ball
+    {
+        private double r;
+
+        public double GetV() { return 4 / 3 * Math.PI * Math.Pow(r, 3); }
+
+        public double GetD() { return r * 2; }
+
+        public double GetS() { return 4 * Math.PI * Math.Pow(r, 2); }
+
+
+        public void Info()
+        {
+            String str =
+                "           * *        \n" +
+                "       *         *      \n" +
+                "     *             *     \n" +
+                "    *      ШАР      *     \n" +
+                "     *             *     \n" +
+                "       *         *      \n" +
+                "           * *        \n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("R = {0:0.00}", r));
+            Console.WriteLine(string.Format("D = {0:0.00}", GetD()));
+            Console.WriteLine(string.Format("V = {0:0.00}", GetV()));
+            Console.WriteLine(string.Format("S = {0:0.00}", GetS()));
+        }
+
+        public void Load()
+        {
+            Console.WriteLine("Введите значение радиуса");
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"ball_output.txt");
+            var new_in = new StreamReader(@"ball_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Ball b = new Ball();
+            b.Load();
+            b.Info();
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
+
