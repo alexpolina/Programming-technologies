d933a77349738099dd9a9e21d34edce7fc590078
Polina Alekseeva <72174553+alexpolina@users.noreply.github.com>
2020-11-18 01:36:47 +0300

    Lab7
    Лабораторная 7

---
+++ Lab7/Plot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Plot
+    {
+        private double a, b;
+
+        public Plot()
+        {
+        }
+
+        public Plot(double pA, double pB)
+        {
+            a = pA; b = pB;
+        }
+
+        public static Plot CreatePlotFromFile()
+        {
+            double aa = Convert.ToDouble(Console.ReadLine());
+            double bb = Convert.ToDouble(Console.ReadLine());
+            return new Plot(aa, bb);
+        }
+
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetIntegral()
+        {
+            double result;
+            result = (b * b / 2) - (a * a / 2);
+            return result;
+        }
+
+        public double GetLength()
+        {
+            double length;
+            length = Math.Sqrt(Math.Pow(b - a, 2) + Math.Pow(a - b, 2));
+            return length;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("График функции y = x");
+            Console.WriteLine(string.Format("Точки a = {0:0.00}, b = {1:0.00}", a, b));
+            Console.WriteLine(string.Format("Опредленный интеграл от a до b равен {0:0.00}", this.GetIntegral()));
+            Console.WriteLine(string.Format("Расстояние между точками (a, y(a)) и (b, y(b)) равно {0:0.00}", this.GetLength()));
+            Console.WriteLine();
+        }
+
+        public void Info(ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Info();
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"plot-output.txt");
+            var new_in = new StreamReader(@"plot-input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in); 
+#endif
+            Plot p1, p2;
+#if DEBUG
+            p1 = new Plot(8, 23);
+            p1.Info(ConsoleColor.Red, ConsoleColor.White);
+#endif
+
+#if !DEBUG
+            p1 = Plot.CreatePlotFromFile();
+            p1.Info();
+            p2 = new Plot(5, 11);
+            p2.Info();
+#endif
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
